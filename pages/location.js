bcdf8904a1c4cd4c676ee0cc5db9c7af53ae1737
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-16 10:51:30 +0530

    add location page with increasing number

--- pages/location.js
+++ pages/location.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import CountUp from 'react-countup';
 function location() {
   return (
-    <div>location</div>
+  <>
+   
+    <div className=' w-full mt-[5rem] max-w-[1280px] mx-auto sm:h-[185px]'>
+      <div className=' flex flex-col sm:flex-row justify-between items-center sm:mx-[206px]  mx-auto text-[56px] sm:text-[62px] text-[#006AFF] font-[300] font-square-regular'>
+        <div >
+       <p className=' leading-[1] '> <CountUp  end={464} /></p>
+       <p className=' font-sans text-[1rem] text-black'>Open jobs globally</p>
+        </div>
+        <div className='mt-[30px]'>
+        <p className=' leading-[1] '><CountUp  decimals={2} end={3.10} />k</p>
+        <p className=' font-sans text-[1rem] text-black'>Open jobs globally</p>
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
 
